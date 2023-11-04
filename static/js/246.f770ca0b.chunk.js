b8aabc31835e75039e3c8533a3cef6cef596e451
astasiempre <126401584+astasiempre@users.noreply.github.com>
2023-11-04 20:38:14 +0000

    Deploying to gh-pages from @ astasiempre/goit-react-hw-08-phonebook@feea77cd597cce7c67ebe9ba01e7676e2bb5285e 🚀

---
+++ static/js/246.f770ca0b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[246],{246:function(e,t,a){a.r(t);a(791);var r=a(184);t.default=function(){return(0,r.jsx)("div",{children:"ContactsPage"})}}}]);
+//# sourceMappingURL=246.f770ca0b.chunk.js.map
